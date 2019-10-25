2cce675cbd410b2f77c69d8224f70e9ab396fcd9
Blaze Hadzik <hadzikblazej@gmail.com>
2019-10-25 15:40:31 +1100

    feat: init commit, IGA stores scraper for morph.io

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,43 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+# frozen_string_literal: true
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'mechanize'
+require 'scraperwiki'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+HEADERS = {
+  'HTTP_USER_AGENT': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13',
+  'HTTP_ACCEPT': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+  'Content-Type': 'text/html; charset=UTF-8'
+}.freeze
+
+PER_PAGE = 100
+BASE_URL = 'http://our-stores.iga.com.au/jm-ajax/get_listings'
+
+agent = Mechanize.new
+
+# 1. Retrieve number of pages to be scraped
+raw_response = agent.get("#{BASE_URL}?page=0&per_page=#{PER_PAGE}")
+parsed_json = JSON.parse(raw_response.body)
+pages_count = parsed_json.fetch('max_num_pages')
+
+# 2. Scrape and profit
+(0..pages_count).each do |page_number|
+  raw_page = agent.get("#{BASE_URL}?page=#{page_number}&per_page=#{PER_PAGE}")
+  json = JSON.parse(raw_page.body)
+  raw_html = Nokogiri::HTML(json.fetch('html'))
+  available_stores = raw_html.search('.job_listing')
+
+  available_stores.each do |store_row|
+    attributes = store_row.attributes
+
+    record = {
+      external_id: attributes.fetch('data-id').value,
+      name: attributes.fetch('data-title').value,
+      link: attributes.fetch('data-link').value,
+      address: attributes.fetch('data-address').value,
+      latitude: attributes.fetch('data-latitude', nil)&.value, # this is optional but nice to have
+      longitude: attributes.fetch('data-longitude', nil)&.value # this is optional but nice to have
+    }
+
+    ScraperWiki.save_sqlite([:external_id], record)
+  end
+end
